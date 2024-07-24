b41bbd60afb3c57a496993d8f078b9bf8e6b92e1
DIGIART <divyshah1907@gmail.com>
2024-07-24 11:34:21 +0530

    divi

--- db/config.js
+++ db/config.js
@@ -13,35 +13,3 @@ mongoose.connect(dbHOST, { useNewUrlParser: true, useUnifiedTopology: true })
         console.error('Error while connecting to MongoDB:', err);
     });
 
-
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
-// dotenv.config();  // Load environment variables from .env file
-
-// const dbHOST = process.env.DBHOST;
-
-// mongoose.connect(dbHOST)
-//     .then(() => {
-//         console.log('MongoDB Connected...');
-//     })
-//     .catch((err) => {
-//         console.error('Error while connecting to MongoDB:', err);
-//     });
-
-
-// const mongoose = require('mongoose');
-// const dotenv = require('dotenv');
-
-// dotenv.config();
-
-// mongoose.connect(process.env.DBHOST, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     serverSelectionTimeoutMS: 30000, // 30 seconds
-//     socketTimeoutMS: 30000 // 30 seconds
-// }).then(() => {
-//     console.log('MongoDB Connected');
-// }).catch((err) => {
-//     console.error('Error connecting to MongoDB:', err);
-// });
