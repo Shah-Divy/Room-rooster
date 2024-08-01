dfa9ea807d077ab6f89d7d4343c668659e79db09
DIGIART <divyshah1907@gmail.com>
2024-08-01 15:43:04 +0530

    images

--- db/Detail.js
+++ db/Detail.js
@@ -1,5 +1,74 @@
-const { type } = require('@testing-library/user-event/dist/cjs/utility/type.js');
-const mongoose = require('mongoose');
+// const { type } = require('@testing-library/user-event/dist/cjs/utility/type.js');
+// const mongoose = require('mongoose');
+
+// const detailSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: true
+//     },
+//     price: {
+//         type: Number,
+//         required: true
+//     },
+//     description: {
+//         type: String,
+//         required: true
+//     },
+//     image: {
+//         data: Buffer,
+//         contentType: String
+//     },
+//     phoneNumber: {
+//         type: String,
+//         required: true
+//     },
+//     sqft: {
+//         type: Number,
+//         required: true
+//     },
+//     bed: {
+//         type: Number,
+//         required: true
+//     },
+//     bath: {
+//         type: Number,
+//         required: true
+//     },
+//     info: {
+//         type: String,
+//         required: true
+//     },
+//     ownername: {
+//         type: String,
+//         required: true
+//     },
+//     FurnishedStatus:{
+//         type: String,
+//         required: true
+//     },
+//     Perferredfor: {
+//         type: String,
+//         required: true
+//     },
+//     ageofconstruction: {
+//         type: Number,
+//         required: true
+//     },
+//     deposit: {
+//         type: Number,
+//         required: true
+//     },
+//     Availability: {
+//         type: String,
+//         required: true
+//     }
+// });
+
+// const Detail = mongoose.model('Detail', detailSchema);
+
+// module.exports = Detail;
+
+
 
 const detailSchema = new mongoose.Schema({
     name: {
@@ -14,10 +83,10 @@ const detailSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    image: {
+    images: [{
         data: Buffer,
         contentType: String
-    },
+    }],
     phoneNumber: {
         type: String,
         required: true
